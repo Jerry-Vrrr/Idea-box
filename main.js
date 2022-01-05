5d03fb9ff8702dd3e0dec8b98f631815873e21ed
Geena Jackson <gjacks303@gmail.com>
2022-01-05 16:43:07 -0700

    Add functionality to removing card from page

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
 
 var star = document.querySelector('.star')
 var starActive = document.querySelector('.star-active')
-var ideaCardsSection = document.querySelector('.idea-cards')
+var ideaCardsSection = document.querySelector('.idea-cards-section')
 
 var deleteButton = document.querySelector('.delete')
 var deleteButtonActive = document.querySelector('.delete-active')
+var ideaCard = document.querySelector('.idea-card')
 
 // var activeDelete= document.querySelector(.'delete-active');
 // var commentPlus = document.querySelector(.'comment-plus');
@@ -29,5 +30,6 @@ function cardSwitch() {
   if (event.target.className === 'delete') {
     deleteButton.classList.add('hidden')
     deleteButtonActive.classList.remove('hidden')
+    ideaCard.classList.add('hidden')
   }
 }
