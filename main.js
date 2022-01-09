1571524987544b0f7da8d61508943383e1d611d5
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-01-08 19:02:43 -0600

    remove white space, un-used variables,functions,pseudocode and adjust to fit js style guide

--- main.js
+++ main.js
@@ -3,30 +3,18 @@ var currentIdeas = []
 
 /* ~~~~~ 👇🏼 Query Selectors Go Here 👇🏼 ~~~~~ */
 
-//Star Buttons
-var star = document.querySelector('.star');
-var starActive = document.querySelector('.star-active');
-var ideaCardsSection = document.querySelector('.idea-cards-section');
-
-//Delete Buttons
-var deleteButton = document.querySelector('.delete');
-var deleteButtonActive = document.querySelector('.delete-active');
-var ideaCard = document.querySelector('.idea-card');
-var commentPlus = document.querySelector('.comment-plus');
-
-//Top Section
+//Form Section
 var ideaTitle = document.querySelector('.idea-card-title');
 var ideaBody = document.querySelector('.idea-card-body');
 var searchIdea = document.querySelector('.search-ideas-bar');
 var saveButton = document.querySelector('.save-button');
 var titleForm = document.querySelector('.title-form');
 var bodyForm = document.querySelector('.body-form');
-//Left section
-var showStarredIdeasButton = document.querySelector('.show-starred-ideas-button');
+
+//Card Section
+var ideaCardsSection = document.querySelector('.idea-cards-section');
 
 /* ~~~~~ 👇🏼 Event Listeners Go Here 👇🏼 ~~~~~ */
-// star.addEventListener('click', cardSwitch)
-//a button that feels for the star click and loads "your getThisThingOn" function
 
 ideaCardsSection.addEventListener('click', favoriteIdea)
 saveButton.addEventListener('click', saveCard)
@@ -36,24 +24,24 @@ bodyForm.addEventListener('input', enableSaveButton)
 /* ~~~~~ 👇🏼 Functions Go Here 👇🏼 ~~~~~ */
 
 function saveCard () {
-    event.preventDefault()
-    var idea = new Idea(titleForm.value, bodyForm.value);
-    currentIdeas.unshift(idea)
-    clearField()
-    displayCard()
+  event.preventDefault()
+  var idea = new Idea(titleForm.value, bodyForm.value);
+  currentIdeas.push(idea)
+  clearField()
+  displayCard()
 }
 
 function clearField(){
-    titleForm.value = ''
-    bodyForm.value = ''
-    saveButton.classList.add('disabled')
-    ideaCardsSection.innerHTML = '';
+  titleForm.value = ''
+  bodyForm.value = ''
+  saveButton.classList.add('disabled')
+  ideaCardsSection.innerHTML = '';
 }
 
 function displayCard() {
   ideaCardsSection.innerHTML = ''
-    for (var i = 0; i < currentIdeas.length; i ++) {
-      if (!currentIdeas[i].starred) {
+  for (var i = 0; i < currentIdeas.length; i ++) {
+    if (!currentIdeas[i].starred) {
     ideaCardsSection.innerHTML += `
     <section class="idea-cards-section" id="${currentIdeas[i].id}">
       <div class="idea-card" id="${currentIdeas[i].id}">
@@ -93,28 +81,17 @@ function displayCard() {
     }
 }
 
-function enableSaveButton() {
-    if (titleForm.value && bodyForm.value){
-    saveButton.classList.remove('disabled')
-    }
-}
-
 function favoriteIdea(event) {
   for (var i = 0; i < currentIdeas.length; i++){
     if (event.target.parentElement.id == currentIdeas[i].id){
-      console.log('banana')
-          currentIdeas[i].updateIdea(currentIdeas[i])
+      currentIdeas[i].updateIdea(currentIdeas[i])
     }
   }
   displayCard()
 }
 
-/* ~~~~~ 👇🏼 Hide/Show Functions Go Here 👇🏼 ~~~~~ */
-
-function hide(element) {
-    element.classList.add('hidden')
-}
-
-function show(element) {
-    element.classList.remove('hidden')
+function enableSaveButton() {
+  if (titleForm.value && bodyForm.value){
+    saveButton.classList.remove('disabled')
+    }
 }
