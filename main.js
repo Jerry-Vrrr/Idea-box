31950a43746f50406410ed992288f19c35eb718b
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-01-10 15:31:17 -0600

    Refactor main.js file

--- main.js
+++ main.js
@@ -23,52 +23,52 @@ bodyForm.addEventListener('input', enableSaveButton)
 
 /* ~~~~~ 👇🏼 Functions Go Here 👇🏼 ~~~~~ */
 
-function saveCard () {
+function saveCard() {
   event.preventDefault()
-  var idea = new Idea(titleForm.value, bodyForm.value);
+  var idea = new Idea(titleForm.value, bodyForm.value)
   currentIdeas.push(idea)
   clearField()
   displayCard()
 }
 
-function clearField(){
+function clearField() {
   titleForm.value = ''
   bodyForm.value = ''
   saveButton.classList.add('disabled')
-  ideaCardsSection.innerHTML = '';
+  ideaCardsSection.innerHTML = ''
 }
 
 function displayCard() {
   ideaCardsSection.innerHTML = ''
-  for (var i = 0; i < currentIdeas.length; i ++) {
+  for (var i = 0; i < currentIdeas.length; i++) {
     if (!currentIdeas[i].starred) {
     ideaCardsSection.innerHTML += `
-      <div class="idea-card">
-        <header class="card-header">
-          <img class="star" src="assets/star.svg" id="${currentIdeas[i].id}"/>
-          <img class="delete" src="assets/delete.svg" id="${currentIdeas[i].id}"/>
-          <img class="delete-active hidden" src="assets/delete-active.svg" id="${currentIdeas[i].id}"/>
+      <div class='idea-card'>
+        <header class='card-header'>
+          <img class='star' src='assets/star.svg' id='${currentIdeas[i].id}' />
+          <img class='delete' src='assets/delete.svg' id='${currentIdeas[i].id}' />
+          <img class='delete-active hidden' src='assets/delete-active.svg' id='${currentIdeas[i].id}' />
         </header>
-        <h1 class="idea-card-title">${currentIdeas[i].title}</h1>
-        <h5 class="idea-card-body">${currentIdeas[i].body}</h5>
+        <h1 class='idea-card-title'>${currentIdeas[i].title}</h1>
+        <h5 class='idea-card-body'>${currentIdeas[i].body}</h5>
         <footer class='card-footer'>
-          <img class="comment-plus" src="assets/comment.svg" />
-          <label class="comment">Comment</label>
+          <img class='comment-plus' src='assets/comment.svg' />
+          <label class='comment'>Comment</label>
         </footer>
       </div>`
     } else {
     ideaCardsSection.innerHTML += `
-      <div class="idea-card">
-        <header class="card-header">
-          <img class="star-active" src="assets/star-active.svg" id="${currentIdeas[i].id}" />
-          <img class="delete" src="assets/delete.svg" id="${currentIdeas[i].id}"/>
-          <img class="delete-active hidden" src="assets/delete-active.svg" id="${currentIdeas[i].id}"/>
+      <div class='idea-card'>
+        <header class='card-header'>
+          <img class='star-active' src='assets/star-active.svg' id='${currentIdeas[i].id}' />
+          <img class='delete' src='assets/delete.svg' id='${currentIdeas[i].id}' />
+          <img class='delete-active hidden' src='assets/delete-active.svg' id='${currentIdeas[i].id}' />
         </header>
-        <h1 class="idea-card-title">${currentIdeas[i].title}</h1>
-        <h5 class="idea-card-body">${currentIdeas[i].body}</h5>
+        <h1 class='idea-card-title'>${currentIdeas[i].title}</h1>
+        <h5 class='idea-card-body'>${currentIdeas[i].body}</h5>
         <footer class='card-footer'>
-          <img class="comment-plus" src="assets/comment.svg" />
-          <label class="comment">Comment</label>
+          <img class='comment-plus' src='assets/comment.svg' />
+          <label class='comment'>Comment</label>
         </footer>
       </div>`
     }
@@ -76,8 +76,7 @@ function displayCard() {
 }
 
 function favoriteIdea(event) {
-  if (event.target.className == "star" || "star-active")
-  for (var i = 0; i < currentIdeas.length; i++){
+  for (var i = 0; i < currentIdeas.length; i++) {
     if (event.target.id == currentIdeas[i].id) {
       currentIdeas[i].updateIdea(currentIdeas[i])
     }
@@ -87,7 +86,7 @@ function favoriteIdea(event) {
 
 function deleteIdea(event) {
   var deleteButton = document.querySelector('.delete')
-  if (event.target.className == "delete")
+  if (event.target.className == 'delete')
   for (var i = 0; i < currentIdeas.length; i++) {
     if (event.target.id == currentIdeas[i].id) {
       currentIdeas.splice(i, 1);
