5d32fc869ba4c398b643790427531552b9c8d018
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-01-10 15:29:40 -0600

    refactor Idea.js

--- Idea.js
+++ Idea.js
@@ -1,18 +1,16 @@
 class Idea {
   constructor(title,body) {
-    this.title = title;
-    this.body = body;
-    this.starred = false;
+    this.title = title
+    this.body = body
+    this.starred = false
     this.id = Date.now()
   }
 
-  updateIdea(idea){
-    // if the star is white then
-    if (!idea.starred){
-    idea.starred = true
+  updateIdea(idea) {
+    if (!idea.starred) {
+      idea.starred = true
   } else {
       idea.starred = false
     }
   }
-
 }
